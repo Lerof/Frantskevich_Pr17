5c9b836e9ca69cc0373154562bbbba201f3b4155
Леонид Францкевич <volt-2014@list.ru>
2022-05-22 21:47:34 +0300

    Добавьте файлы проекта.

---
+++ Frantskevich_Pr17/Frantskevich_Pr17/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Frantskevich_Pr17
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ButtonResult_Clicked(object sender, EventArgs e)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                lableResult.TextColor = Color.Black;
+                string req = webClient.DownloadString($"http://numbersapi.com/{entryNumber.Text.Replace(".", "")}?notfound=ceil");
+                lableResult.Text = req;
+            }
+        }
+    }
+}
